779602cb47f481e10d9eb0a383c9b070b1432e09
Dreadrith <73959477+Dreadrith@users.noreply.github.com>
2020-11-23 21:30:31 +0200

    Add files via upload

---
+++ Selection Helper/SaveSelection.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class SaveSelection : Editor
+{
+    //By Dreadrith#3238
+    //https://discord.gg/ZsPfrGn
+
+    static Object[] oldSelection;
+
+    [MenuItem("Assets/Selection Helper/Save Selection")]
+    [MenuItem("GameObject/Selection Helper/Save Selection", false, 10)]
+    static void saveSelected()
+    {
+        oldSelection = Selection.GetFiltered<Object>(SelectionMode.OnlyUserModifiable);
+    }
+
+    [MenuItem("Assets/Selection Helper/Load Selection")]
+    [MenuItem("GameObject/Selection Helper/Load Selection", false, 10)]
+    static void loadSelected()
+    {
+        if (oldSelection != null)
+            Selection.objects = oldSelection;
+    }
+
+    [MenuItem("Assets/Selection Helper/Load Selection (ADD)")]
+    [MenuItem("GameObject/Selection Helper/Load Selection (ADD)", false, 10)]
+    static void loadSelectedC()
+    {
+        if (oldSelection != null)
+            Selection.objects = Selection.objects.Concat(oldSelection).ToArray();
+    }
+}
+#endif
---
+++ Selection Helper/SelectDependencies.cs
@@ -0,0 +1,85 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+
+public class SelectDependencies : EditorWindow
+{
+    //By Dreadrith#3238
+    //https://discord.gg/ZsPfrGn
+
+    [MenuItem("Assets/Selection Helper/Select Dependencies/No Shaders or Scripts", false, -10)]
+   private static void selectDependenciesNeither()
+    {
+        Object[] myAssets = Selection.GetFiltered<Object>(SelectionMode.Assets);
+        HashSet<Object> files = new HashSet<Object>();
+        string[] ignoreExt = {".cs",".shader",".dll" };
+        foreach (Object obj in myAssets)
+           foreach(string path in AssetDatabase.GetDependencies(AssetDatabase.GetAssetPath(obj)))
+            {
+                bool ignore = false;
+                string myExt = path.Substring(path.LastIndexOf('.'),path.Length- path.LastIndexOf('.'));
+                foreach (string ext in ignoreExt)
+                    if (myExt == ext)
+                    {
+                        ignore = true;
+                        break;
+                    }
+
+                if (!ignore)
+                    files.Add(AssetDatabase.LoadAssetAtPath<Object>(path));
+            }
+        Selection.objects = files.ToArray();
+    }
+
+    [MenuItem("Assets/Selection Helper/Select Dependencies/No Shaders", false, -10)]
+    private static void selectDependenciesShader()
+    {
+        Object[] myAssets = Selection.GetFiltered<Object>(SelectionMode.Assets);
+        HashSet<Object> files = new HashSet<Object>();
+        string[] ignoreExt = {".shader" };
+        foreach (Object obj in myAssets)
+            foreach (string path in AssetDatabase.GetDependencies(AssetDatabase.GetAssetPath(obj)))
+            {
+                bool ignore = false;
+                string myExt = path.Substring(path.LastIndexOf('.'), path.Length - path.LastIndexOf('.'));
+                foreach (string ext in ignoreExt)
+                    if (myExt == ext)
+                    {
+                        ignore = true;
+                        break;
+                    }
+                if (!ignore)
+                    files.Add(AssetDatabase.LoadAssetAtPath<Object>(path));
+            }
+        Selection.objects = files.ToArray();
+    }
+
+    [MenuItem("Assets/Selection Helper/Select Dependencies/No Scripts", false, -10)]
+    private static void selectDependenciesSript()
+    {
+        Object[] myAssets = Selection.GetFiltered<Object>(SelectionMode.Assets);
+        HashSet<Object> files = new HashSet<Object>();
+        string[] ignoreExt = { ".cs", ".dll" };
+        foreach (Object obj in myAssets)
+            foreach (string path in AssetDatabase.GetDependencies(AssetDatabase.GetAssetPath(obj)))
+            {
+                bool ignore = false;
+                string myExt = path.Substring(path.LastIndexOf('.'), path.Length - path.LastIndexOf('.'));
+                foreach (string ext in ignoreExt)
+                    if (myExt == ext)
+                    {
+                        ignore = true;
+                        break;
+                    }
+
+                if (!ignore)
+                    files.Add(AssetDatabase.LoadAssetAtPath<Object>(path));
+            }
+        Selection.objects = files.ToArray();
+    }
+}
+#endif
