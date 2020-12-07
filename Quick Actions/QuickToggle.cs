89ddf5d9e37125c080d3ea11cc66c812bf117e9d
Dreadrith <73959477+Dreadrith@users.noreply.github.com>
2020-12-07 15:32:42 +0200

    Upgrade Quick Toggle
    Supports Multiple objects with individual Enable/Disable
    Auto clip naming
    Choose saving path
    Better Root finding
    Better issue handling
    Minor extra options

--- Quick Actions/QuickToggle.cs
+++ Quick Actions/QuickToggle.cs
@@ -1,74 +1,120 @@
 ﻿#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
-using VRC.SDK3.Avatars.Components;
 using DS_CommonMethods;
+using System.Collections.Generic;
+using System.Linq;
+
+#if VRC_SDK_VRCSDK3
+using VRC_AvatarDescriptor = VRC.SDK3.Avatars.Components.VRCAvatarDescriptor;
+#elif VRC_SDK_VRCSDK2
+using VRC_AvatarDescriptor = VRCSDK2.VRC_AvatarDescriptor;
+#endif
+
 
 public class QuickToggle : EditorWindow
 {
     public static GameObject root;
-    public static GameObject myObject;
-    public static bool enable=true, disable;
-    public static bool gesture;
+    public static List<ToggleObject> targets = new List<ToggleObject>();
+    public static UnityEditorInternal.ReorderableList targetList;
+    public static bool gesture, autoName,pingClip;
+    public static string clipName;
 
     private static GUIContent warnIcon;
-    private static bool notChild, noRoot, noObject,noToggle;
-    private static string myPath;
+    private static GUIContent arrowIcon;
+    private static GUIContent greenLight, redLight;
+    private static GUIContent switchIcon;
+    
 
     private static bool init;
+    private static string myPath;
+    private static bool clipValid = false;
+    private static Vector2 scroll;
 
-    [MenuItem("GameObject/Quick Actions/Toggle Clip", false, -10)]
-    public static void showWindow(MenuCommand selected)
+    [MenuItem("GameObject/Quick Actions/Quick Toggle", false, -10)]
+    public static void ShowWindow()
     {
-        QuickToggle win = GetWindow<QuickToggle>(false, "Quick Toggle", true);
-        win.maxSize = new Vector2(400, 160);
-        win.minSize = new Vector2(400, 160);
-        GameObject dummyObj = selected.context as GameObject;
-        if (dummyObj)
-        {
-            myObject = dummyObj;
-            if (!root)
+        targets.Clear();
+        GetWindow<QuickToggle>(false, "Quick Toggle", true);
+        GameObject[] targetObjs = Selection.GetFiltered<GameObject>(SelectionMode.OnlyUserModifiable);
+        for (int i = 0; i < targetObjs.Length; i++)
+            targets.Add(new ToggleObject(targetObjs[i]));
+        if (!root)
+            try
             {
-                try { root = myObject.transform.root.GetComponentInChildren<VRCAvatarDescriptor>(true).gameObject; } catch { root = myObject.transform.root.gameObject; };
+                root = Selection.GetFiltered<GameObject>(SelectionMode.TopLevel).First().transform.root.GetComponentInChildren<VRC_AvatarDescriptor>(true).gameObject;
             }
+            catch {
+                root = Selection.GetFiltered<GameObject>(SelectionMode.TopLevel).First().transform.root.gameObject;
+            }
+        if (autoName)
+        {
+            clipName = "";
+            if (targetObjs.Length == 0)
+                clipName = "Objects";
+            else
+                if (targetObjs.Length == 1)
+                clipName = targetObjs[0].name;
+            else
+                for (int i = 0; i < targetObjs.Length; i++)
+                {
+                    if (targetObjs[i].name.Length < 2)
+                        continue;
+                    clipName += targetObjs[i].name.Substring(0, 2).ToUpper();
+                    if (i != targetObjs.Length - 1)
+                        clipName += "-";
+                }
+
+            clipName += " Toggle";
         }
-        checkIfValid();
-        GUI.FocusControl("CreateClip");
+        CheckIfValid();
         init = false;
-    }
+    } 
 
     private void OnGUI()
     {
+        if (!init)
+            RefreshList();
+
+        scroll = EditorGUILayout.BeginScrollView(scroll);
+
         EditorGUI.BeginChangeCheck();
         root = (GameObject)EditorGUILayout.ObjectField("Root", root, typeof(GameObject), true);
-        EditorGUILayout.BeginHorizontal();
-        myObject = (GameObject)EditorGUILayout.ObjectField("Target", myObject, typeof(GameObject), true);
-        if (notChild)
-        {
-            EditorGUILayout.LabelField(warnIcon, GUILayout.Width(20));
-            EditorGUILayout.EndHorizontal();
-            EditorGUILayout.HelpBox(myObject.name + " is not a child of " + root.name + "!", MessageType.Warning);
-        }
-        else
-            EditorGUILayout.EndHorizontal();
-        
-        EditorGUILayout.BeginHorizontal();
-        enable = EditorGUILayout.ToggleLeft("Enable", enable);
-        disable = EditorGUILayout.ToggleLeft("Disable", disable);
-        EditorGUILayout.EndHorizontal();
-        gesture = EditorGUILayout.ToggleLeft("Is Gesture", gesture);
         if (EditorGUI.EndChangeCheck())
         {
-            checkIfValid();
+            CheckIfValid();
         }
-        EditorGUI.BeginDisabledGroup(noRoot || noObject || notChild || noToggle);
+        EditorGUILayout.BeginHorizontal();
+        clipName = EditorGUILayout.TextField("Clip Name", clipName);
+        EditorGUIUtility.labelWidth = 70;
+        EditorGUI.BeginChangeCheck();
+        autoName = EditorGUILayout.Toggle(new GUIContent("AutoName","Automatically generate a clip name when using context menu button"), autoName,GUILayout.Width(100));
+        if (EditorGUI.EndChangeCheck())
+            PlayerPrefs.SetInt("QuickToggleAutoName", autoName ? 1 : 0);
+        EditorGUIUtility.labelWidth = 0;
+        EditorGUILayout.EndHorizontal();
+        targetList.DoLayoutList();
+        EditorGUI.BeginChangeCheck();
+        gesture = EditorGUILayout.Toggle(new GUIContent("Is Gesture","Generates curves in 2 frames rather than 1 and sets loop time to true."), gesture);
+        if (EditorGUI.EndChangeCheck())
+            PlayerPrefs.SetInt("QuickToggleIsGesture", gesture ? 1 : 0);
+        EditorGUI.BeginChangeCheck();
+        pingClip = EditorGUILayout.Toggle(new GUIContent("Ping Clip", "Automatically highlights the newly generated clip in Assets"), pingClip);
+        if (EditorGUI.EndChangeCheck())
+            PlayerPrefs.SetInt("QuickTogglePingClip", pingClip ? 1 : 0);
+        EditorGUI.BeginDisabledGroup(!clipValid || string.IsNullOrWhiteSpace(clipName));
         GUI.SetNextControlName("CreateClip");
         if (GUILayout.Button("Create Clip"))
-            makeClip();
+            CreateClip();
         EditorGUI.EndDisabledGroup();
+
+        DSCommonMethods.AssetFolderPath(ref myPath, "Clips Path", "QuickTogglePath");
+       
         EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-        DSCommonMethods.Credit();
         
+        DSCommonMethods.Credit();
+        EditorGUILayout.EndScrollView();
+
         if (!init)
         {
             GUI.FocusControl("CreateClip");
@@ -76,77 +122,167 @@ public class QuickToggle : EditorWindow
         }
 
         if (GUI.GetNameOfFocusedControl() == "CreateClip" && Event.current.type == EventType.KeyDown && (Event.current.keyCode == KeyCode.Return || Event.current.keyCode == KeyCode.KeypadEnter))
-            makeClip();
+            CreateClip();
     }
 
-    static void makeClip()
+    static void CreateClip()
     {
-        if (!AssetDatabase.IsValidFolder(myPath + "/Generated Clips"))
-            AssetDatabase.CreateFolder(myPath, "Generated Clips");
-        string assetFolder = myPath + "/Generated Clips/" + root.name;
-        if (!AssetDatabase.IsValidFolder(assetFolder))
-            AssetDatabase.CreateFolder(myPath+ "/Generated Clips",root.name);
-        string path = AnimationUtility.CalculateTransformPath(myObject.transform, root.transform);
-        if (enable)
+        DSCommonMethods.RecreateFolders(myPath);
+        AnimationClip myClip = new AnimationClip();
+        foreach (ToggleObject obj in targets)
         {
-            AnimationClip enableClip = new AnimationClip();
+            if (obj==null)
+                continue;
+            string path = AnimationUtility.CalculateTransformPath(obj.Obj.transform, root.transform);
+
             if (!gesture)
-                enableClip.SetCurve(path, typeof(GameObject), "m_IsActive", new AnimationCurve { keys = new Keyframe[] { new Keyframe { time = 0, value = 1 } } });
+                myClip.SetCurve(path, typeof(GameObject), "m_IsActive", new AnimationCurve { keys = new Keyframe[] { new Keyframe { time = 0, value = obj.active ? 1 : 0 } } });
             else
-            {
-                enableClip.SetCurve(path, typeof(GameObject), "m_IsActive", new AnimationCurve { keys = new Keyframe[] { new Keyframe { time = 0, value = 1 }, new Keyframe { time = 1f / 60f, value = 1 } } });
-                AnimationClipSettings settings = AnimationUtility.GetAnimationClipSettings(enableClip);
-                settings.loopTime = true;
-                AnimationUtility.SetAnimationClipSettings(enableClip, settings);
-            }
-            string clipPath = assetFolder + "/" + myObject.name + " Enable.anim";
-            if (!AssetDatabase.LoadAssetAtPath<AnimationClip>(clipPath))
-                AssetDatabase.CreateAsset(enableClip, clipPath);
-            else
-                Debug.Log(myObject.name + " Enable.anim already exists");
+                myClip.SetCurve(path, typeof(GameObject),  "m_IsActive" , new AnimationCurve { keys = new Keyframe[] { new Keyframe { time = 0, value = obj.active ? 1 : 0 }, new Keyframe { time = 1f / 60f, value = obj.active ? 1 : 0 } } });
         }
-        if (disable)
+        if (gesture)
         {
-            AnimationClip disableClip = new AnimationClip();
-            if (!gesture)
-                disableClip.SetCurve(path, typeof(GameObject), "m_IsActive", new AnimationCurve { keys = new Keyframe[] { new Keyframe { time = 0, value = 0 } } });
-            else
-            {
-                disableClip.SetCurve(path, typeof(GameObject), "m_IsActive", new AnimationCurve { keys = new Keyframe[] { new Keyframe { time = 0, value = 0 }, new Keyframe { time = 1f / 60f, value = 0 } } });
-                AnimationClipSettings settings = AnimationUtility.GetAnimationClipSettings(disableClip);
-                settings.loopTime = true;
-                AnimationUtility.SetAnimationClipSettings(disableClip, settings);
-            }
-            string clipPath = assetFolder + "/" + myObject.name + " Disable.anim";
-            if (!AssetDatabase.LoadAssetAtPath<AnimationClip>(clipPath))
-                AssetDatabase.CreateAsset(disableClip, clipPath);
-            else
-                Debug.Log(myObject.name + " Disable.anim already exists");
+            AnimationClipSettings settings = AnimationUtility.GetAnimationClipSettings(myClip);
+            settings.loopTime = true;
+            AnimationUtility.SetAnimationClipSettings(myClip, settings);
         }
-        EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>(assetFolder));
-        Debug.Log(myObject.name +" Clips Created.");
+        string clipPath = AssetDatabase.GenerateUniqueAssetPath(myPath + "/"+clipName+ ".anim");
+        AssetDatabase.CreateAsset(myClip, clipPath);
+        if (pingClip)
+            EditorGUIUtility.PingObject(AssetDatabase.LoadAssetAtPath<Object>(clipPath));
+        Debug.Log(clipPath.Substring(clipPath.LastIndexOf('/')+1,clipPath.Length-clipPath.LastIndexOf('/')-6) +" Created.");
     }
 
-    private static void checkIfValid()
+    private static void CheckIfValid()
     {
-        noRoot = !root;
-        noObject = !myObject;
-        if (!noRoot && !noObject)
-            notChild = ( myObject==root || !myObject.transform.IsChildOf(root.transform));
-        else
-            notChild = false;
-        noToggle = !enable && !disable;
+        bool validated = root;
+        if (root)
+            foreach (ToggleObject obj in targets)
+                if (obj.Obj && (!obj.Obj.transform.IsChildOf(root.transform)))
+                {
+                    obj.valid = false;
+                    validated = false;
+                }
+                else
+                    obj.valid = true;
+        for (int i = 0; i < targets.Count; i++)
+        {
+            if (targets[i].Obj)
+            {
+                goto EndValidation;
+            }
+
+        }
+        validated = false;
+
+    EndValidation:
+        clipValid = validated;
     }
 
 
     private void OnEnable()
     {
-        warnIcon = EditorGUIUtility.IconContent("d_console.warnicon.sml");
-        myPath = this.GetPath();
-        checkIfValid();
+        warnIcon = new GUIContent(EditorGUIUtility.IconContent("d_console.warnicon.sml")) {tooltip="Object is not a child of Root!" };
+        arrowIcon = new GUIContent(EditorGUIUtility.IconContent("d_refresh")) {tooltip="Cycle Component"};
+        greenLight = new GUIContent(EditorGUIUtility.IconContent("d_greenLight")) {tooltip="Enable"};
+        redLight = new GUIContent(EditorGUIUtility.IconContent("d_redLight")) {tooltip="Disable"};
+        switchIcon = new GUIContent(EditorGUIUtility.IconContent("d_Animation.Record")) {tooltip = "Invert Toggles"};
+
+        myPath = PlayerPrefs.GetString("QuickTogglePath", "Assets/DreadScripts/Quick Actions/Quick Toggle/Generated Clips");
+        autoName = PlayerPrefs.GetInt("QuickToggleAutoName", 1) == 1;
+        pingClip = PlayerPrefs.GetInt("QuickTogglePingClip", 1) == 1;
+        gesture = PlayerPrefs.GetInt("QuickToggleIsGesture", 0) == 1;
+        RefreshList();
+        CheckIfValid();
         init = false;
     }
 
+    private void DrawHeader(Rect rect)
+    {
+        EditorGUI.LabelField(rect, "Targets");
+
+        if (GUI.Button(new Rect(rect.width-10,rect.y+2,20,EditorGUIUtility.singleLineHeight),switchIcon,GUIStyle.none))
+        {
+            foreach (ToggleObject obj in targets)
+                obj.active = !obj.active;
+        }
+    }
+
+    private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
+    {
+        if (!(index < targets.Count && index >= 0))
+            return;
+        if (GUI.Button(new Rect(rect.x, rect.y + 2, 20, EditorGUIUtility.singleLineHeight), "X"))
+        {
+            targets.RemoveAt(index);
+            CheckIfValid();
+            return;
+        }
+
+        ToggleObject toggleObj = targets[index];
+        Rect myRect = new Rect(rect.x + 22, rect.y + 2, rect.width - 42, EditorGUIUtility.singleLineHeight);
+        EditorGUI.BeginChangeCheck();
+
+        Object dummy;
+            dummy = EditorGUI.ObjectField(myRect, toggleObj.Obj, typeof(GameObject), true);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            if (dummy == null)
+                targets[index] = new ToggleObject();
+            else
+            {
+                if (((GameObject)dummy).scene.IsValid())
+                {
+                    targets[index] = new ToggleObject((GameObject)dummy);
+                }
+                else
+                    Debug.LogWarning("[QuickToggle] GameObject must be a scene object!");
+            }
+            CheckIfValid();
+        }
+        float xCoord = rect.x + rect.width - 18;
+        if (!toggleObj.valid)
+            EditorGUI.LabelField(new Rect(xCoord - 40, rect.y+2, 25, EditorGUIUtility.singleLineHeight), warnIcon);
+
+        if (toggleObj.active)
+            if (GUI.Button(new Rect(xCoord, rect.y, 20, 18), greenLight, GUIStyle.none))
+            {
+                toggleObj.active = false;
+            }
+        if (!toggleObj.active)
+            if (GUI.Button(new Rect(xCoord, rect.y, 20, 18), redLight, GUIStyle.none))
+            {
+                toggleObj.active = true;
+            }
+    
+    }
+
+    private void RefreshList()
+    {
+        targetList = new UnityEditorInternal.ReorderableList(targets, typeof(ToggleObject), false, true, true, false)
+        {
+            drawElementCallback = DrawElement,
+            drawHeaderCallback = DrawHeader
+        };
+    }
+
+    public class ToggleObject
+    {
+        public GameObject Obj = null;
+        public bool active = true;
+        public bool valid = true;
+        public ToggleObject() { }
+
+        public ToggleObject(GameObject obj)
+        {
+            Obj = obj;
+        }
+    }
 }
+
+
+    
+
 
 #endif
